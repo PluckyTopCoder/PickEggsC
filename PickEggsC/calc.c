988bcf3ea87d33122b68e2a39233c140e28d6557
Li Li <9085903@qq.com>
2017-03-01 16:14:50 +0800

    Initial version - C program to calculate eggs in basket.

---
+++ PickEggsC/calc.c
@@ -0,0 +1,31 @@
+/*
+ * calc.c
+ *
+ *  Created on: 2017年2月20日
+ *      Author: lili
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int i = 9;
+	while (1) {
+		if (i % 1 == 0 &&
+			i % 2 == 1 &&
+			i % 3 == 0 &&
+			i % 4 == 1 &&
+			i % 5 == 1 &&
+			i % 6 == 3 &&
+			i % 7 == 0 &&
+			i % 8 == 1 &&
+			i % 9 == 0) {
+
+			break;
+		}
+
+		i++;
+	}
+
+	printf("%d", i);
+	return 0;
+}
